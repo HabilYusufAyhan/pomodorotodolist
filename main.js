7b5ee489b82a7a212ed8ff4927eab7d54342d895
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2022-12-08 23:17:45 +0300

    update

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
 // görev 27 satır
+const time = document.querySelectorAll('.time');
 const missiontext = document.querySelectorAll(".missiontext");
-const cross = document.querySelector(".cross");
-const mission = document.querySelector('.mission');
-const missiontime = document.querySelectorAll('.missiontime')
-mission.addEventListener('click',silme)
-missiontext.forEach(function(text) {
-    console.log(text.textContent.length);
-    if (text.textContent.length > 28) {
-        text.style.fontSize = '20px'
+const cross = document.querySelectorAll(".cross");
+const mission = document.querySelectorAll(".mission");
+const missiontime = document.querySelectorAll(".missiontime");
+const controlbutton = document.querySelectorAll(".controlbutton");
+const startmission = document.querySelectorAll(".startmission");
+const finishmission = document.querySelector(".finishmission");
+cross.forEach(function (tick) {
+  tick.onclick = function () {
+    if (tick.classList.contains("fa-x")) {
+      console.log("tıklandı");
+      tick.parentElement.parentElement.parentElement.parentElement.remove();
+      const addmissionli = document.createElement("li");
+      addmissionli.classList.add("mission");
+      newtext = tick.parentElement.parentElement.parentElement.textContent;
+      totaltime =
+        tick.parentElement.parentElement.parentElement.parentElement
+          .lastElementChild.firstElementChild.firstElementChild.textContent;
+        
+          newtour =
+        tick.parentElement.parentElement.parentElement.parentElement
+          .lastElementChild.firstElementChild.lastElementChild.textContent;
+      newbreaking =
+        tick.parentElement.parentElement.parentElement.parentElement
+          .lastElementChild.lastElementChild.lastElementChild.textContent;
+       
+      addmissionli.innerHTML = `
+ <div class="textcontrol">
+ <span class="missiontext" style="text-align: left; max-width: 320px;"
+   >${newtext}</span
+ >
+</div>
+ <div class="missiontime">
+   <p>Süre: <span class="time">${totaltime}</span><span class="tour">${newtour}</span></p>
+   <p>Mola Süresi: <span class="breakingtime">${newbreaking}</span></p>
+ </div>
+`;
+      finishmission.appendChild(addmissionli);
     }
-    if (text.textContent.length > 33) {
-        text.style.fontSize = '15px'
-    }
-    if (text.textContent.length>42) {
-        text.style.fontSize = '13px'
-    }
-    if (text.textContent.length>48) {
-        text.style.fontSize = '10px'
-    }
+  };
   
 });
-function silme(e) {
-   
-        const tiklanilan = e.target;
-        if (tiklanilan.classList.contains('cross')) {
-            console.log('tiklandi');
-        }
-      
-}
+
 
 
